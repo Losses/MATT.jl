d760210c87b0d561fb324d14d3ce7c5f1ddc5cc0
Losses Don <losses.don@gmail.com>
2019-08-20 19:28:08 +0800

    fix: Fix typing.

--- src/React.jl
+++ src/React.jl
@@ -7,6 +7,8 @@ JSON.lower(x::UUID) = string(x)
     tag::String = "div"
     props::Union{Dict{String, Any}, Nothing} = nothing
     children::Union{Vector{JSXElement}, Nothing} = nothing
+    data_type::Union{DataType, Nothing} = nothing
+    component_type::String #Input? Output? Static?
 end
 
 # BindSet -> Input
@@ -18,32 +20,43 @@ end
 end
 
 function parse_tree(x::InputComponent)
-    local update_rules::Vector{UpdateRule} = []
+    local update_rules = Vector{UpdateRule}()
+    local components = Vector{Component}()
+
+    append!(components, [x])
 
     local jsx_tree = JSXElement(
         hash = x.hash,
         tag = x.component,
         props = x.parameters,
-        children = nothing
+        children = nothing,
+        component_type = "input",
+        data_type = typeof(x).parameters[1]
     )
 
     return ParsedJSXTree(
         jsx_tree = jsx_tree,
-        update_rules = update_rules
+        update_rules = update_rules,
+        components = components
     )
 end
 
 function parse_tree(x::OutputComponent)
-    local update_rules::Vector{UpdateRule} = []
+    local update_rules = Vector{UpdateRule}()
+    local components = Vector{Component}()
+
+    append!(components, [x])
 
     local jsx_tree = JSXElement(
         hash = x.hash,
         tag = x.component,
         props = x.parameters,
-        children = nothing
+        children = nothing,
+        component_type = "output",
+        data_type = typeof(x).parameters[1]
     )
 
-    local inputs::Vector{UUID} = []
+    local inputs = Vector{UUID}()
 
     for bind in x.callback.bind_set.binds
         append!(inputs, [bind.component.hash])
@@ -57,26 +70,32 @@ function parse_tree(x::OutputComponent)
 
     return ParsedJSXTree(
         jsx_tree = jsx_tree,
-        update_rules = update_rules
+        update_rules = update_rules,
+        components = components
     )
 end
 
 @with_kw struct ParsedJSXTree
     jsx_tree::JSXElement
     update_rules::Vector{UpdateRule}
+    components::Vector{Component}
 end
 
 function parse_tree(x::StaticComponent)
-    local update_rules::Vector{UpdateRule} = []
+    local update_rules = Vector{UpdateRule}()
+    local components = Vector{Component}()
+
+    append!(components, [x])
 
     if !isnothing(x.children)
-        local children::Vector{JSXElement} = []
+        local children = Vector{JSXElement}()
 
         for i in x.children
             local parsed_tree = parse_tree(i)
 
             append!(update_rules, parsed_tree.update_rules)
             append!(children, [parsed_tree.jsx_tree])
+            append!(components, [i])
         end
     else
         local children = nothing
@@ -86,27 +105,32 @@ function parse_tree(x::StaticComponent)
         hash = x.hash,
         tag = x.component,
         props = x.parameters,
-        children = children
+        children = children,
+        component_type = "static",
+        data_type = nothing
     )
 
     return ParsedJSXTree(
         jsx_tree = jsx_tree,
-        update_rules = update_rules
+        update_rules = update_rules,
+        components = components
     )
 end
 
 @with_kw struct MATTApp
     hash::UUID = uuid4()
     jsx_tree::JSXElement
-    update_rules::Vector{UpdateRule}
+    input_components::Dict{UUID, InputComponent}
+    output_components::Dict{UUID, OutputComponent}
+    update_rules::Dict{UUID, Vector{UpdateRule}}
     input_bind::Dict{UUID, Vector{UUID}}
     bind_input::Dict{UUID, Vector{UUID}}
-    serialized_app::String
     bind_output::Dict{UUID, Vector{UUID}}
+    serialized_app::String
 end
 
 function setup_app(x::ParsedJSXTree)
-    setup_app(x.jsx_tree, x.update_rules)
+    setup_app(x.jsx_tree, x.update_rules, x.components)
 end
 
 function setup_app(x::StaticComponent)
@@ -115,7 +139,8 @@ end
 
 function setup_app(
     jsx_tree::JSXElement,
-    update_rules::Vector{UpdateRule})
+    update_rules::Vector{UpdateRule},
+    components::Vector{T} where T <: Component)
 
     local input_bind = Dict{UUID, Vector{UUID}}()
     local bind_input = Dict{UUID, Vector{UUID}}()
