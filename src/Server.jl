4564d897e4ca0f6f1a4a3e8c195e6c5d3611c4e0
Losses Don <losses.don@gmail.com>
2019-08-24 17:07:46 +0800

    feat: Some debug information.

--- src/Server.jl
+++ src/Server.jl
@@ -52,25 +52,72 @@ macro validate_input(conn, condition, error_msg)
 end
 
 function ws_io(x, app::MATTApp)
+    local connection_hash = uuid4()
     local conn = x[:socket]
+
+    @info "Incomming connection" connection_hash
 
     while !eof(conn)
         local update_hash = uuid4()
         local data = String(WebSockets.readguarded(conn)[1])
         local parsed_data = try JSON.parse(data) catch e; end
 
+        @info "Received message" update_hash parsed_data
+
         @validate_input(conn, isnothing(parsed_data), "Failed to parse the request to JSON")
         @validate_input(conn, !(typeof(parsed_data) <: Dict), "The request should be an Object")
         @validate_input(conn, !haskey(parsed_data, "command"), "No command provided")
 
+        @info "Validated the message" update_hash
+
         if parsed_data["command"] == "update"
+
+            @info "Matched command" update_hash
+
+            """
+            Client side should pass an update command in the following format:
+
+            {
+              "command": "update",
+              "bind_set": "<UUID of bind set>",
+              "input": {
+                "<UUID of input>": "<value of input>",
+                "...": "..."
+              }
+            }
+
+            Example requirement:
+
+            ```
+            {
+              "command": "update",
+              "bind_set": "4c274ad0-9373-4e08-84b1-9a86f9aba145",
+              "input": {
+                "bdea432c-e94f-4def-ab43-6587beb93607": true
+              }
+            }
+            ```
+
+            Error messages:
+            * **1-02-001**: No bind set provided, provide a UUID of the bind set;
+            * **1-02-002**: Invalid bind set hash, will be thrown while faild to parse the bind set hash to UUID;
+            * **1-02-003**: Bind set not found, the bind set client side provided is not defined in the MATT app;
+            * **1-02-004**: No input provided, will be thrown while no input field found in the request;
+            * **1-02-005**: Input should be an object, the input field should be a Javascript Object, but something else provided;
+            * **1-02-006**: Mismatch input type, the input parameters provided can't be converted to predefinded type.
+            """
             @validate_input(conn, !haskey(parsed_data, "bind_set"), "No bind set provided")
             @validate_input(conn, !haskey(parsed_data, "input"), "No input provided")
 
+            @info "validated the input" update_hash
+
             local bind_set_hash = try UUID(parsed_data["bind_set"]) catch e; end
+
             @validate_input(conn, isnothing(bind_set_hash), "Invalid bind set hash, should be an UUID")
             @validate_input(conn, !haskey(app.bind_output, bind_set_hash), "Bind set not found")
             @validate_input(conn, !(typeof(parsed_data["input"]) <: Dict{String, Any}), "Input should be an Object")
+
+            @info "validated bind set" update_hash
 
             local bind_set = app.bind_sets[bind_set_hash]
             local paras_valid = true
@@ -100,6 +147,8 @@ function ws_io(x, app::MATTApp)
 
             @validate_input(conn, !paras_valid, "Mismatch input type")
 
+            @info "validated input" update_hash
+
             local output_hashes = app.bind_output[bind_set.hash]
 
             for output_hash in output_hashes
@@ -117,6 +166,8 @@ function ws_io(x, app::MATTApp)
 
                 WebSockets.writeguarded(conn, response)
             end
+        else
+            @validate_input(conn, true, "Invalid command")
         end
     end
 end
@@ -137,6 +188,8 @@ function run_app(x::MATTApp; port::Int64 = 2333)
         route("/MATT-io", WS_IO(x)),
         Mux.wclose,
         Mux.notfound());
+
+    @info "MATT initialized"
 
     WebSockets.serve(
         WebSockets.ServerWS(
