5dc07d616fd6b1b4cf7efd86ff85e4221401c7a1
Losses Don <losses.don@gmail.com>
2019-08-31 17:01:25 +0800

    fix: Typing problem of uiStore.

--- src/client/src/redux/uiStore.ts
+++ src/client/src/redux/uiStore.ts
@@ -1,9 +1,10 @@
 import *  as React from 'react';
 
 import store, { HashTable } from './main';
+import { UPDATE_INPUT } from './inputStore';
 import FabricComponents from '../components/Fabric';
 
-export type UIStatus = 'success' | 'error' | 'fetching' | 'not-ready';
+export type UIStatus = 'success' | 'error' | 'fetching' | 'not-ready' | 'updating';
 
 interface NotReadyUIStore {
   status: 'not-ready';
@@ -19,7 +20,7 @@ interface ErrorUIStore {
 }
 
 interface SuccessUIStore {
-  status: 'success';
+  status: 'success' | 'updating';
   element: React.ReactElement<any>;
   input_bind: HashTable;
   bind_input: HashTable;
@@ -36,13 +37,15 @@ export interface MATTAppDefinition {
 export interface JSXTreeDefinition {
   hash: string;
   tag: string;
-  props: { [hash: string]: any }
-  children?: JSXTreeDefinition[]
+  component_type: 'input' | 'output' | 'static';
+  props: { [hash: string]: any };
+  children?: JSXTreeDefinition[];
 }
 
 export const START_FETCH_UI = 'start-fetch-ui';
 export const REPORT_FETCH_UI_ERROR = 'report-fetch-ui-error';
 export const UPDATE_UI = 'update-ui';
+export const FINISH_UPDATE_UI = 'finish-update-ui';
 
 interface StartFetchAction {
   type: 'start-fetch-ui';
@@ -60,7 +63,11 @@ interface UpdateUIAction {
   bind_input: HashTable;
 }
 
-export type UIAction = StartFetchAction | ReportErrorAction | UpdateUIAction;
+interface FinishUpdateUI {
+  type: 'finish-update-ui';
+}
+
+export type UIAction = StartFetchAction | ReportErrorAction | UpdateUIAction | FinishUpdateUI;
 
 const INITIAL_UI_STORE: UIStore = {
   status: 'not-ready'
@@ -69,7 +76,7 @@ const INITIAL_UI_STORE: UIStore = {
 export const uiReducer = (
   store: UIStore = INITIAL_UI_STORE,
   action: UIAction
-):UIStore => {
+): UIStore => {
   switch (action.type) {
     case START_FETCH_UI:
       return {
@@ -82,38 +89,57 @@ export const uiReducer = (
       }
     case UPDATE_UI:
       return {
-        status: 'success',
+        status: 'updating',
         element: action.element,
         input_bind: action.input_bind,
         bind_input: action.bind_input
       }
+    case FINISH_UPDATE_UI:
+      return Object.assign(store, {
+        status: 'success'
+      })
     default:
       return store
   }
 }
 
 const parseJsxTreeDefinition = (x: JSXTreeDefinition): React.ReactElement => {
+  let props;
   const children = x.children ? x.children.map(parseJsxTreeDefinition) : null;
-  const props = { ...x.props, __hash: x.hash };
 
-  return React.createElement(FabricComponents[x.tag], props, children)
+  props = { ...x.props, __hash: x.hash };
+
+  if (x.component_type == 'input') {
+    const handleOnChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (!event.target) return false
+      store.dispatch({ type: UPDATE_INPUT, input: x.hash, value: event.target.value })
+    }
+
+    props = Object.assign(props, { onChange: handleOnChange });
+
+    store.dispatch({ type: UPDATE_INPUT, input: x.hash, value: x.props.value })
+  }
+
+  return React.createElement(FabricComponents[x.tag], props, children);
 }
 
 export const getInitialUI = async () => {
   try {
-    store.dispatch({ type: 'start-fetch-ui' });
+    store.dispatch({ type: START_FETCH_UI });
     const ui_response = await fetch('/ui');
     const ui = await ui_response.json() as MATTAppDefinition;
 
     store.dispatch({
-      type: 'update-ui',
+      type: UPDATE_UI,
       element: parseJsxTreeDefinition(ui.jsx_tree),
       input_bind: ui.input_bind,
       bind_input: ui.bind_input,
     });
+
+    store.dispatch({ type: FINISH_UPDATE_UI });
   } catch (e) {
     store.dispatch({
-      type: 'report-fetch-ui-error',
+      type: REPORT_FETCH_UI_ERROR,
       error: e.message
     })
   }
